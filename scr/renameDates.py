d9e09b865cc404f4f00f3624988df607d87899a6
joyc <joyc@live.jp>
2017-03-21 01:24:58 +0900

    update mcb.pyw

--- scr/renameDates.py
+++ scr/renameDates.py
@@ -12,14 +12,29 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
     (.*?)$                          # all text after the date
     """, re.VERBOSE)
 
-# TODO: Loop over the files in the working directory.
+# Loop over the files in the working directory.
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
 
-# TODO: Skip files without a date.
+    # Skip files without a date.
+    if mo == None:
+        continue
 
-# TODO: Get the different parts of the filename.
+    # Get the different parts of the filename.
+    beforePart = mo.group(1)
+    monthPart  = mo.group(2)
+    dayPart    = mo.group(4)
+    yearPart   = mo.group(6)
+    afterPart  = mo.group(8)
 
-# TODO: Form the European-style filename.
+    # Form the European-style filename.
+    euroFilename = beforePart + dayPart + '-' + monthPart + '-' + yearPart + afterPart
 
-# TODO: Get the full, absolute file paths.
+    # Get the full, absolute file paths.
+    absWorkingDir = os.path.abspath('.')
+    amerFilename = os.path.join(absWorkingDir, amerFilename)
+    euroFilename = os.path.join(absWorkingDir, euroFilename)
 
-# TODO: Rename the files.
+    # Rename the files.
+    print('Renaming "%s" to "%s"...' % (amerFilename, euroFilename))
+    #shutil.move(amerFilename, euroFilename) # uncomment after testing
