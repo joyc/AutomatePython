bb9610a97e87f01315b3f9ea529d5b8aa22153cc
joyc <joyc@live.jp>
2017-03-21 00:53:28 +0900

    update mcb.pyw

--- scr/renameDates.py
+++ scr/renameDates.py
@@ -8,6 +8,7 @@ import shutil, os, re
 datePattern = re.compile(r"""^(.*?) # all text before the date
     ((0|1)?/d) -                    # one or two digits for the month
     ((0|1|2|3)?/d) -                # one or two digits for the day
+    ((19|20)\d\d)                   # four digits for the year
     (.*?)$                          # all text after the date
     """, re.VERBOSE)
 
@@ -16,3 +17,9 @@ datePattern = re.compile(r"""^(.*?) # all text before the date
 # TODO: Skip files without a date.
 
 # TODO: Get the different parts of the filename.
+
+# TODO: Form the European-style filename.
+
+# TODO: Get the full, absolute file paths.
+
+# TODO: Rename the files.
