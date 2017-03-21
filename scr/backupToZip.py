d503c3196be8121ae68082621656a801c5bb876c
joyc <joyc@live.jp>
2017-03-22 00:11:51 +0900

    update backupToZip.py

--- scr/backupToZip.py
+++ scr/backupToZip.py
@@ -19,8 +19,23 @@ def backupToZip(folder):
             break
         number += 1
 
-    # TODO: Create the ZIP file.
-    # TODO: Walk the entire folder tree and compress the files in each folder.
+    # Create the ZIP file.
+    print('Creating %s...' % (zipFilename))
+    backupZip = zipfile.ZipFile(zipFilename, 'w')
+
+    # Walk the entire folder tree and compress the files in each folder.
+    for foldername, subfolders, filenames in os.walk(folder):
+        print('Add the files in %s...' % (foldername))
+        # Add the current folder to the ZIP file.
+        backupZip.write(foldername)
+
+        # Add all the files in this folder to the ZIP file.
+        for filename in filenames:
+            if filename.startswith(os.path.basename(folder) + '_') and \
+                filename.endswith('.zip'):
+                continue    # don't backup the backup ZIP files
+            backupZip.write(os.path.join(foldername, filename))
+    backupZip.close()
     print('Done.')
 
 backupToZip('C:\\Python\\MyScripts\\testzip')
