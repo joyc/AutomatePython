efae7c5cf477d8d25c75113dc0a5061c9bc5794f
joyc <joyc@live.jp>
2017-03-22 00:14:24 +0900

    update backupToZip.py

--- scr/backupToZip.py
+++ scr/backupToZip.py
@@ -6,34 +6,33 @@
 import zipfile, os
 
 def backupToZip(folder):
-    # Backup the entire contents of "folder" into a ZIP file.
+    # Backup the entire contents of "folder" into a zip file.
 
-    folder = os.path.abspath(folder)    # make sure folder is absolute
+    folder = os.path.abspath(folder) # make sure folder is absolute
 
-    # Figure out the filename this code should use based on
+    # Figure out the filename this code should used based on
     # what files already exist.
     number = 1
     while True:
         zipFilename = os.path.basename(folder) + '_' + str(number) + '.zip'
         if not os.path.exists(zipFilename):
             break
-        number += 1
+        number = number + 1
 
-    # Create the ZIP file.
+    # Create the zip file.
     print('Creating %s...' % (zipFilename))
     backupZip = zipfile.ZipFile(zipFilename, 'w')
 
     # Walk the entire folder tree and compress the files in each folder.
     for foldername, subfolders, filenames in os.walk(folder):
-        print('Add the files in %s...' % (foldername))
+        print('Adding files in %s...' % (foldername))
         # Add the current folder to the ZIP file.
         backupZip.write(foldername)
 
         # Add all the files in this folder to the ZIP file.
         for filename in filenames:
-            if filename.startswith(os.path.basename(folder) + '_') and \
-                filename.endswith('.zip'):
-                continue    # don't backup the backup ZIP files
+            if filename.startswith(os.path.basename(folder) + '_') and filename.endswith('.zip'):
+                continue # don't backup the backup ZIP files
             backupZip.write(os.path.join(foldername, filename))
     backupZip.close()
     print('Done.')
